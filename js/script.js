00bac21fc6bffb95b24ffb87d6d7a31f6ae42bf0
simven <56834754+simven@users.noreply.github.com>
2019-11-27 19:02:40 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('load',function(){
+  document.getElementById('main-id').classList.remove('isLoading');
+});
+
+
+jQuery(function () {
+  "use strict";
+  $(function () {
+      $(window).scroll(function () {  //Fonction pour déscendre la page
+          if ($(this).scrollTop() > 200) {  // A 200pixels du haut de page
+              $('#scrollUp').css('right', '30px');  // Replace l'image à 30pixels à droite
+          } else {
+              $('#scrollUp').removeAttr('style'); // Supprime les attributs CSS affectés par javascript
+          }
+      });
+  });
+
+  $("#scrollUp").click(function () {
+    $("html, body").animate({scrollTop: 0}, "slow");
+  });
+
+  $(document).ready(function () {
+    $('a[href^="#"]').on('click', function (evt) {
+        evt.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body')
+            .stop()
+            .animate({scrollTop: $(target).offset().top -45}, 1000);
+    });
+  });
+
+
+  // Ferme le menu responsive quand on clique sur un lien
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+  
+
+  // Réduit le menu
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 60) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+
+  // Réduit si la page n'est pas en haut
+  navbarCollapse();
+
+  // Réduit le menu quand on scroll la page
+  $(window).scroll(navbarCollapse);
+  
+});
