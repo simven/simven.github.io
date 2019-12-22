bf2b146d20aa3d38caa58076ff1aeedbf57ee2df
simven <56834754+simven@users.noreply.github.com>
2019-12-22 05:32:28 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,59 +4,53 @@ window.addEventListener('load',function(){
 });
 
 
-jQuery(function () {
-  "use strict";
-  $(function () {
-      $(window).scroll(function () {  //Fonction pour déscendre la page
-          if ($(this).scrollTop() > 200) {  // A 200pixels du haut de page
-              $('#scrollUp').css('right', '30px');  // Replace l'image à 30pixels à droite
-          } else {
-              $('#scrollUp').removeAttr('style'); // Supprime les attributs CSS affectés par javascript
-          }
-      });
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
   });
 
-  $("#scrollUp").click(function () {
-    $("html, body").animate({scrollTop: 0}, "slow");
-  });
-
-  $(document).ready(function () {
-    $('a[href^="#"]').on('click', function (evt) {
-        evt.preventDefault();
-        var target = $(this).attr('href');
-        $('html, body')
-            .stop()
-            .animate({scrollTop: $(target).offset().top -90}, 1000);
-    });
-  });
-
-
-
-  // Ferme le menu responsive quand on clique sur un lien
+  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  
+  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 75
+    offset: 56
   });
-  
 
-  // Réduit le menu
+  // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 60) {
-      $("#mainNav").addClass("navbar-scrolled");
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-scrolled");
+      $("#mainNav").removeClass("navbar-shrink");
     }
   };
-
-  // Réduit si la page n'est pas en haut
+  // Collapse now if page is not at top
   navbarCollapse();
-
-  // Réduit le menu quand on scroll la page
+  // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-  
-});
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+})(jQuery); // End of use strict
