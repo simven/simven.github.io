4422a3090a1a74193d7dbe51df51328df48f4a20
Simon Vennat <56834754+simven@users.noreply.github.com>
2020-06-16 17:53:48 +0200

    cv

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('load',function(){
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
-
+  
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
